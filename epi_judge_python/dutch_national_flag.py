70b645c5398e5de996863ca9fea25c2d247c600d
Mihail Andreev <mikeandreev@gmail.com>
2018-11-23 09:29:58 +0300

    dutch flag

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -6,9 +6,31 @@ from test_framework.test_utils import enable_executor_hook
 
 RED, WHITE, BLUE = range(3)
 
+#
+# [DONE]
+# variant to try : sorting when key in 4 valued enum
+#
 
 def dutch_flag_partition(pivot_index, A):
-    # TODO - you fill in here.
+    if len(A) <= 1:
+        return
+    pivot = A[pivot_index]
+    after_ltp = 0 # first index after less_then_pivot part
+    last_unp = len(A)-1  # last index of unprcessed part
+    cur = after_ltp
+    
+    while cur <= last_unp:
+    	if A[cur] < pivot:
+    		if after_ltp < cur:
+    			A[after_ltp], A[cur] = A[cur], A[after_ltp]
+    		after_ltp += 1
+    		cur += 1
+    	elif A[cur] > pivot:
+    		A[last_unp], A[cur] = A[cur], A[last_unp]
+    		last_unp -= 1
+    	else: # A[cur] == pivot
+    	    cur += 1
+    
     return
 
 
