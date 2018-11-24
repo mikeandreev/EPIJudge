da7967aa8789a5ed063a6e1f656749b0645fde62
Mihail Andreev <mikeandreev@gmail.com>
2018-11-24 20:57:33 +0300

    done

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -1,8 +1,14 @@
 from test_framework import generic_test
 
+# DONE
 
 def can_reach_end(A):
-    # TODO - you fill in here.
+    max_reachable = 0
+    for i in range(len(A)):
+        if i > max_reachable: return False
+        if i+A[i] >= len(A): return True
+        if i+A[i] > max_reachable: max_reachable = i+A[i]
+
     return True
 
 
@@ -10,3 +16,11 @@ if __name__ == '__main__':
     exit(
         generic_test.generic_test_main(
             "advance_by_offsets.py", "advance_by_offsets.tsv", can_reach_end))
+            
+            
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        
+    def test1(self):
+        self.assertEqual( can_reach_end([3, 3, 1, 0, 3, 1, 0, 2, 0, 1]), True)
--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -1,10 +1,28 @@
 from test_framework import generic_test
 import copy
 
-# WIP : brute force is done
+# DONE
 
 def multiply(num1, num2):
-    return []
+    return multiply2(num1, num2)
+    
+def multiply2(A, B):
+    neg = (A[0] < 0) ^ (B[0] < 0)
+    A[0], B[0] = abs(A[0]), abs(B[0])
+    res = [0]*(len(A)+len(B))
+    for i in reversed( range( len(A)) ):
+        for j in reversed( range( len(B)) ):
+            k = i + j + 1
+            res[k] += A[i] *B[j]
+            res[k-1] += res[k] // 10
+            res[k] %= 10
+    # python trick:
+    res = res[ next( ( i for i, x in enumerate(res) if x != 0 ), len(res)  ) : ] or [0]
+    
+    if neg:
+        res[0] *= -1
+    return res
+
 #####
 # >>>
 def plus( A, B):
@@ -36,8 +54,8 @@ def multiply1(num1, num2):
     if num1 == [0] or num2 == [0]:
         return [0]
     res = []
-    neg = num1[0] * num2[0] < 0
-    num1[0] = abs(num1[0]); num2[0] = abs(num2[0])
+    neg = (num1[0] < 0) ^ (num2[0] < 0)
+    num1[0], num2[0] = abs(num1[0]), abs(num2[0])
     for i in range( len(num2) ):
         tmp = mult( copy.deepcopy(num1), num2[i] ) + [0]*(len(num2)-1-i)
         res = plus( res, tmp)
@@ -47,9 +65,6 @@ def multiply1(num1, num2):
 #<<<<
 ####
 
-# trick:
-# result[ next( ( i for i, x in enumerate(result) if x != 0 ), len(result)  ) : ] or [0]
-
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("int_as_array_multiply.py",
@@ -58,24 +73,25 @@ if __name__ == '__main__':
 import unittest
 
 class TestStringMethods(unittest.TestCase):
+    
+    def hlp_multiply(self, f):
+        self.assertEqual( f([1, 9, 3, 7, 0, 7, 7, 2, 1], [1]),       [1, 9, 3, 7, 0, 7, 7, 2, 1] )
+        self.assertEqual( f([1, 9, 3, 7, 0, 7, 7, 2, 1], [1, 0]), [1, 9, 3, 7, 0, 7, 7, 2, 1, 0] )
+        self.assertEqual( f([1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 0]), [3, 8, 7, 4, 1, 5, 4, 4, 2, 0] )
+        self.assertEqual( f([1, 9, 3, 7, 0, 7, 7, 2, 1], [1, 1]), [2, 1, 3, 0, 7, 8, 4, 9, 3, 1] )
+        self.assertEqual( f([1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 1]), [4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
+        self.assertEqual( f([1, 9, 3, 7, 0, 7, 7, 2, 1], [-2, 1]), [-4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
+        self.assertEqual( f([-1, 9, 3, 7, 0, 7, 7, 2, 1], [-2, 1]), [4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
+        self.assertEqual( f([-1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 1]), [-4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
+        self.assertEqual( f([2, 6, 7], [0]), [0])
+    
     def test_multiply1(self):
-        self.assertEqual( multiply1([1, 9, 3, 7, 0, 7, 7, 2, 1], [1]),       [1, 9, 3, 7, 0, 7, 7, 2, 1] )
-        self.assertEqual( multiply1([1, 9, 3, 7, 0, 7, 7, 2, 1], [1, 0]), [1, 9, 3, 7, 0, 7, 7, 2, 1, 0] )
-        self.assertEqual( multiply1([1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 0]), [3, 8, 7, 4, 1, 5, 4, 4, 2, 0] )
-        self.assertEqual( multiply1([1, 9, 3, 7, 0, 7, 7, 2, 1], [1, 1]), [2, 1, 3, 0, 7, 8, 4, 9, 3, 1] )
-        self.assertEqual( multiply1([1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 1]), [4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
-        self.assertEqual( multiply1([1, 9, 3, 7, 0, 7, 7, 2, 1], [-2, 1]), [-4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
-        self.assertEqual( multiply1([-1, 9, 3, 7, 0, 7, 7, 2, 1], [-2, 1]), [4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
-        self.assertEqual( multiply1([-1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 1]), [-4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
+        self.hlp_multiply( multiply1 )
+        
 
-        self.assertEqual( multiply([1, 9, 3, 7, 0, 7, 7, 2, 1], [1]),       [1, 9, 3, 7, 0, 7, 7, 2, 1] )
-        self.assertEqual( multiply([1, 9, 3, 7, 0, 7, 7, 2, 1], [1, 0]), [1, 9, 3, 7, 0, 7, 7, 2, 1, 0] )
-        self.assertEqual( multiply([1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 0]), [3, 8, 7, 4, 1, 5, 4, 4, 2, 0] )
-        self.assertEqual( multiply([1, 9, 3, 7, 0, 7, 7, 2, 1], [1, 1]), [2, 1, 3, 0, 7, 8, 4, 9, 3, 1] )
-        self.assertEqual( multiply([1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 1]), [4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
-        self.assertEqual( multiply([1, 9, 3, 7, 0, 7, 7, 2, 1], [-2, 1]), [-4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
-        self.assertEqual( multiply([-1, 9, 3, 7, 0, 7, 7, 2, 1], [-2, 1]), [4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
-        self.assertEqual( multiply([-1, 9, 3, 7, 0, 7, 7, 2, 1], [2, 1]), [-4, 0, 6, 7, 8, 6, 2, 1, 4, 1] )
+    def test_multiply2(self):
+        self.hlp_multiply( multiply2 )
+        
         
     def test_mult0(self):
         self.assertEqual(mult([1, 1, 1], 5), [5, 5, 5])
