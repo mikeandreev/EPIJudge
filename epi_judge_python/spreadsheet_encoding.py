1781a6e70df30558ec1972d1a6a64289d261c665
Mike Andreev <mikeandreev@gmail.com>
2018-12-19 11:32:37 +0300

    1done

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -1,10 +1,20 @@
 from test_framework import generic_test
 
-# WIP
+# DONE
+import functools
 
 def ss_decode_col_id(col):
-    # TODO - you fill in here.
-    return 0
+    num = 0
+    ordA = ord('A')
+    base = 26 # ord('Z') - ordA + 1
+    #
+    #for c in col:
+    #    num = num*base + (ord(c) - ordA + 1)
+    #return num
+    #
+    return functools.reduce(
+            lambda num, char: num*base + (ord(char) - ordA + 1),
+            col, 0)
 
 
 if __name__ == '__main__':
@@ -12,3 +22,15 @@ if __name__ == '__main__':
         generic_test.generic_test_main("spreadsheet_encoding.py",
                                        'spreadsheet_encoding.tsv',
                                        ss_decode_col_id))
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def test1(self):
+        self.assertEqual( ss_decode_col_id("A"), 1)
+    def test2(self):
+        self.assertEqual( ss_decode_col_id("Z"), 26)
+    def test3(self):
+        self.assertEqual( ss_decode_col_id("AA"), 27)
+    def test4(self):
+        self.assertEqual( ss_decode_col_id("ZZ"), 702)
+
