2c97877ebd3f284db017e1b7c16015e0702321cc
Mihail Andreev <mikeandreev@gmail.com>
2019-05-07 14:24:22 +0000

    1 done

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
+import bisect
+
+# DONE: with lib
+# DONE: from scratch
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    L, U, res = 0, len(A)-1, -1
+    while L <= U:
+        M = L + (U - L) // 2
+        if A[M] < k:
+            L = M + 1
+        elif k < A[M]:
+            U = M - 1
+        else: # A[M] == k
+            res = M
+            U = M - 1
+    return res
+
+def search_first_of_k_with_lib(A, k):
+    i = bisect.bisect_left(A, k)
+    if 0 <= i < len(A) and A[i] == k:
+        return i
+    else:
+        return -1
 
 
 if __name__ == '__main__':
