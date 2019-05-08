b3abd8aa0688c7fc3af6360e07e986ae51cd1759
Mike Andreev <mikeandreev@gmail.com>
2019-05-08 19:09:01 +0300

    1done

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -1,9 +1,36 @@
 from test_framework import generic_test
+import heapq
 
+# DONE
 
 def sort_k_increasing_decreasing_array(A):
-    # TODO - you fill in here.
-    return []
+    INCR, DECR = range(2)
+    order, begin = INCR, 0
+    sub_arr = []
+    for i in range(1, len(A)):
+        if (order == INCR and A[i-1] > A[i]) or (order == DECR and A[i-1] < A[i]):
+            sub_arr.append( iter(A[begin: i]) if order == INCR else reversed( A[begin: i] ) )
+            begin = i
+            order = 1 - order
+    sub_arr.append( iter(A[begin: ]) if order == INCR else reversed( A[begin: ] ) )
+
+    # ver 1:
+    #return list( heapq.merge( *sub_arr ) )
+
+    # ver 2:
+    res, heap = [], []
+    def add_next(i):
+        n = next(sub_arr[i], None)
+        if n is not None: heapq.heappush( heap, (n, i) )
+    for i in range(len(sub_arr)):
+        add_next(i)
+    while heap:
+        n, i = heapq.heappop( heap )
+        res.append( n )
+        add_next(i)
+    return res
+
+
 
 
 if __name__ == '__main__':
