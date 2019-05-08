4eb647ea799afa261a64537b053b2d41cf1c3c9e
Mihail Andreev <mikeandreev@gmail.com>
2019-05-08 11:56:11 +0300

    1 done

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,8 +1,18 @@
 from test_framework import generic_test
 
+from collections import Counter
+
+# DONE
 
 def can_form_palindrome(s):
-    # TODO - you fill in here.
+    return len([True for v in Counter(s).values() if v%2]) <= 1
+
+def can_form_palindrome_1(s):
+    c = Counter(s)
+    has_odd = False
+    for _ in (v for v in c.values() if v%2):
+        if has_odd: return False
+        else: has_odd = True
     return True
 
 
