2fdbf2d658761cd5ffb664d407a98c112b16a829
Mike Andreev <mikeandreev@gmail.com>
2019-05-08 16:56:07 +0300

    3done

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
 
+# DONE
 
 def levenshtein_distance(A, B):
-    # TODO - you fill in here.
-    return 0
+    a, b = len(A)+1, len(B)+1
+    C = [[0]*a for _ in range(b)] # DP cache
+    for i in range(b):
+        for j in range(a):
+            if i == 0: C[i][j] = j
+            elif j == 0: C[i][j] = i
+            else:
+                if A[j-1] == B[i-1]: C[i][j] = C[i-1][j-1]
+                else: C[i][j] = 1 + min(C[i-1][j-1], C[i-1][j], C[i][j-1] )
+    return C[-1][-1]
 
 
 if __name__ == '__main__':
--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -1,9 +1,17 @@
 from test_framework import generic_test
 
+# DONE
 
 def number_of_ways(n, m):
-    # TODO - you fill in here.
-    return 0
+    l, k = n + m - 2, n - 1 # need to calculate (l choose k)
+    if k == 0 or l == k: return 1
+    C = [[0]*(k+1) for _ in range(l+1)] # DP cache, we are using indexes [ from 1 to l ][ 1 to k ]
+    for i in range(1, l+1):
+        for j in range(max(1, i-l+k), min(k+1, i+1) ):
+            if j == i: C[i][j] = 1
+            elif j == 1: C[i][j] = i
+            else: C[i][j] = C[i-1][j] + C[i-1][j-1]
+    return C[-1][-1]
 
 
 if __name__ == '__main__':
