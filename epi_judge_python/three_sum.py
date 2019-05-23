b51ba0ad46c5bdcf6822a02a6b9915a2a02070af
Mihail Andreev <mikeandreev@gmail.com>
2019-05-23 15:18:35 +0300

    solution update

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -6,16 +6,19 @@ import bisect
 
 def has_three_sum(A, t):
     A.sort()
-    def has_two_sum(lo, hi, c):
+    def has_two_sum(lo, c):
+        hi = len(A)-1
         while lo <= hi:
             x = A[lo] + A[hi]
             if x == c: return True
             elif x < c: lo += 1
             else: hi -= 1
         return False
-    for i in range(len(A)):
-        if has_two_sum(i, len(A)-1, t-A[i]): return True
-    return False
+        
+    #for i, a in enumerate(A):
+    #    if has_two_sum(i, t-a): return True
+    #return False
+    return any(has_two_sum(i, t-a) for i, a in enumerate(A))
 
 
 if __name__ == '__main__':
