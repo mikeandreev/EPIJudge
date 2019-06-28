ff7b9096edc0da2dc1b8c59eb0532bc98756fe19
Mihail Andreev <mikeandreev@gmail.com>
2019-06-28 13:26:17 +0000

    1 done, 1 extra solution

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -1,9 +1,13 @@
 from test_framework import generic_test, test_utils
 
+# DONE
 
-def find_k_largest_in_bst(tree, k):
-    # TODO - you fill in here.
-    return None
+def find_k_largest_in_bst_rec(tree, k):
+    if not tree or k == 0: return []
+    found = find_k_largest_in_bst(tree.right, k)
+    if len(found) == k: return found
+    found.append( tree.data )
+    return found + find_k_largest_in_bst(tree.left, k - len(found))
 
 
 if __name__ == '__main__':
--- epi_judge_python/search_first_greater_value_in_bst.py
+++ epi_judge_python/search_first_greater_value_in_bst.py
@@ -1,8 +1,31 @@
 from test_framework import generic_test
 
-# DONE
+# DONE: with recursion 1
+# DONE: with recursion 2
+# DONE: no recursion
 
 def find_first_greater_than_k(tree, k):
+    sub_tree, found = tree, None
+    while sub_tree:
+        if sub_tree.data <= k:
+            sub_tree = sub_tree.right
+        else: # sub_tree.data > k:
+            found = sub_tree
+            sub_tree = sub_tree.left
+    
+    return found
+
+def find_first_greater_than_k_rec2(tree, k):
+    if tree is None: return None
+    if tree.data <= k:
+        return find_first_greater_than_k(tree.right, k)
+    
+    left_res = find_first_greater_than_k(tree.left, k)
+    if left_res is not None: return left_res
+    if tree.data > k: return tree
+    return None
+
+def find_first_greater_than_k_rec1(tree, k):
     if tree is None: return None
     left_res = find_first_greater_than_k(tree.left, k)
     if left_res is not None: return left_res
