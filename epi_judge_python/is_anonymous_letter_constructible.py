d0dda8dbd3fae0df674bbd0857d371c81f4740a8
Mihail Andreev <mikeandreev@gmail.com>
2019-06-28 08:44:15 +0000

    is_anonymous_letter_constructible

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+from collections import Counter
+# DONE: with Counter
+# DONE: using dict
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
-    # TODO - you fill in here.
-    return True
+    stat = {}
+    for c in letter_text:
+        if c not in stat: stat[c] = 1
+        else:             stat[c] += 1
+    for c in magazine_text:
+        if c in stat:
+            val = stat[c]
+            if val == 1:
+                del stat[c]
+                if not stat: return True
+            else: stat[c] -= 1
+    return (not stat)
+
+def is_letter_constructible_from_magazine_wCounter(letter_text, magazine_text):
+    check = Counter(letter_text) - Counter(magazine_text)
+    return (not check) # True if chek is empty 
 
 
 if __name__ == '__main__':
