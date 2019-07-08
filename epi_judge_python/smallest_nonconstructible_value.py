8bf36fd97797009598e0759071efee02e198f047
Mike Andreev <mike.andreev@gmail.com>
2019-07-08 15:22:49 +0000

    smallest_nonconstructible_value

--- epi_judge_python/smallest_nonconstructible_value.py
+++ epi_judge_python/smallest_nonconstructible_value.py
@@ -1,13 +1,37 @@
+import functools
+
 from test_framework import generic_test
+from test_framework.binary_tree_utils import must_find_node
+from test_framework.test_failure import TestFailure
+from test_framework.test_utils import enable_executor_hook
 
-#WIP
+# DONE
 
-def smallest_nonconstructible_value(A):
-    return 0
+# Input nodes are nonempty and the key at s is less than or equal to that at b.
+def find_LCA(tree, s, b):
+    node = tree
+    while node:
+        if node.data > b.data:
+            node = node.left
+        elif node.data < s.data:
+            node = node.right
+        else:
+            return node
+    return None
+
+
+@enable_executor_hook
+def lca_wrapper(executor, tree, s, b):
+    result = executor.run(
+        functools.partial(find_LCA, tree, must_find_node(tree, s),
+                          must_find_node(tree, b)))
+    if result is None:
+        raise TestFailure("Result can't be None")
+    return result.data
 
 
 if __name__ == '__main__':
     exit(
-        generic_test.generic_test_main("smallest_nonconstructible_value.py",
-                                       'smallest_nonconstructible_value.tsv',
-                                       smallest_nonconstructible_value))
+        generic_test.generic_test_main("lowest_common_ancestor_in_bst.py",
+                                       'lowest_common_ancestor_in_bst.tsv',
+                                       lca_wrapper))
