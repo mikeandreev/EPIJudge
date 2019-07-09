61016e8259819200ade2f6f4e77cf79fa58b4eb3
Mihail Andreev <mikeandreev@gmail.com>
2019-07-09 18:25:23 +0300

    2 done

--- epi_judge_python/binomial_coefficients.py
+++ epi_judge_python/binomial_coefficients.py
@@ -1,8 +1,19 @@
 from test_framework import generic_test
 
-# DONE
+# DONE : rec with cache
+# DONE : bottom-up
 
 def compute_binomial_coefficient(n, k):
+    C = [ [0]*min(k+1, i+1) for i in range(n+1)] 
+    def hlp(n, k):
+        if C[n][k] == 0:
+            if k == 0 or n == k: C[n][k] = 1
+            elif k == 1: C[n][k] = n
+            else: C[n][k] = hlp(n-1, k) + hlp(n-1, k-1)
+        return C[n][k]
+    return hlp(n, k)
+
+def compute_binomial_coefficient_1(n, k):
     C = [ [0]*min(k+1, i+1) for i in range(n+1)] 
     for i in range(n+1):
         for j in range(min(k+1, i+1)):
--- epi_judge_python/run_length_compression.py
+++ epi_judge_python/run_length_compression.py
@@ -1,15 +1,32 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+# DONE
 
 def decoding(s):
-    # TODO - you fill in here.
-    return ''
+    res = []
+    n = 0
+    for c in s:
+        if c.isdigit():
+            n = n*10 + int(c)
+        else:
+            res.append( c * n )
+            n = 0
+            
+    return ''.join(res)
 
 
 def encoding(s):
-    # TODO - you fill in here.
-    return ''
+    res = ''
+    n, prev = 1, s[0]
+    for c in s[1:]:
+        if c != prev:
+            res += str(n)+prev
+            n, prev = 1, c
+        else:
+            n += 1
+    res += str(n)+prev
+    return res
 
 
 def rle_tester(encoded, decoded):
