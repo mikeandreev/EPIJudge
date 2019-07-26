36a2817ffcf6977c9957d94b8fa5fe5a20d85e69
Mike Andreev <mikeandreev@gmail.com>
2019-07-26 21:45:42 +0300

    permutations.py

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test, test_utils
 
+# DONE
 
 def permutations(A):
-    # TODO - you fill in here.
-    return []
+    res = []
+    n = len(A)
+    def hlp(L, l):
+        if l == 0:
+            res.append(L.copy())
+            return
+        for i in reversed(range(l)):
+            if l == n:
+                L = L.copy()
+            L[i], L[l-1] = L[l-1], L[i]
+            hlp(L, l-1)
+            #L[i], L[l-1] = L[l-1], L[i]
+
+    hlp(A, n)
+    return res
 
 
 if __name__ == '__main__':
