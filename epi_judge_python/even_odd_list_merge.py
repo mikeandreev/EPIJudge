01925a6a4587014fbbbaeb96f1093ec04fd472a4
Mike Andreev <mikeandreev@gmail.com>
2019-07-26 10:45:47 +0300

    even_odd_list_merge.py

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
+# DONE
 
 def even_odd_merge(L):
-    # TODO - you fill in here.
-    return None
+    if not L: return L
+
+    head, odd_head = L, L.next
+    even_it, odd_it = head, odd_head
+    while odd_it and odd_it.next:
+        # changing links
+        even_it.next, odd_it.next = odd_it.next, odd_it.next.next
+        # moving iterators
+        even_it, odd_it = even_it.next, odd_it.next
+
+    even_it.next = odd_head
+
+    return head
 
 
 if __name__ == '__main__':
