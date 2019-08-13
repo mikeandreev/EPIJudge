d490d70028908910a2bca1a6e2188aac1716ffd5
Mihail Andreev <mikeandreev@gmail.com>
2019-08-13 21:51:23 +0300

    1 done

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
+# DONE
 
 def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+    res = []
+    stack = []
+    while tree or stack:
+        if not tree:
+            tree = stack.pop()
+            res.append(tree.data)
+            tree = tree.right
+        else:
+            stack.append(tree)
+            tree = tree.left
+    return res
+    
+def inorder_traversal_rec(tree):
+    res = []
+    def hlp(tree):
+        if not tree: return
+        hlp(tree.left)
+        res.append(tree.data)
+        hlp(tree.right)
+    hlp(tree)
+    return res
 
 
 if __name__ == '__main__':
