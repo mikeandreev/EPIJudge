a9ab2345cef0c4e24b5ce5060408929b1f8daf8d
Mike Andreev <mikeandreev@gmail.com>
2019-08-29 07:41:30 +0300

    power_set.py

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test, test_utils
 
+# DONE
 
 def generate_power_set(S):
-    # TODO - you fill in here.
-    return []
+    res = [[]]
+    pref = []
+    def hlp(prev_indx):
+        for i in range(prev_indx + 1, len(S)):
+            pref.append(S[i])
+            res.append(pref.copy())
+            if i < len(S)-1:
+                hlp(i)
+            pref.pop()
+    hlp(-1)
+    return res
 
 
 if __name__ == '__main__':
