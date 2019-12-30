550417004cb76ff684b17523e16bf60b1c3e2e01
Mike Andreev <mikeandreev@gmail.com>
2019-12-30 18:58:18 +0300

    2done

--- epi_judge_python/knapsack.py
+++ epi_judge_python/knapsack.py
@@ -6,10 +6,23 @@ from test_framework.test_utils import enable_executor_hook
 
 Item = collections.namedtuple('Item', ('weight', 'value'))
 
+# DONE
 
 def optimum_subject_to_capacity(items, capacity):
-    # TODO - you fill in here.
-    return 0
+    #cache = [ {} for _ in items]
+    cache = [ [-1] * (capacity + 1) for _ in items ]
+    def solve(j, maxW):
+        if maxW <= 0 or j < 0: return 0
+
+        w, v = items[j].weight, items[j].value
+        if cache[j][maxW] >= 0:
+            return cache[j][maxW]
+        res = solve(j-1, maxW)
+        if w <= maxW:
+            res = max(res, solve(j-1, maxW-w) + v)
+        cache[j][maxW] = res
+        return res
+    return solve(len(items)-1, capacity)
 
 
 @enable_executor_hook
--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
 
+import collections
+# DONE
 
 def find_nearest_repetition(paragraph):
-    # TODO - you fill in here.
-    return 0
+    inf = float("inf")
+    prev = collections.defaultdict(lambda: -inf)
+    min_dist = inf
+    for i, word in enumerate(paragraph):
+        dist = i - prev[word]
+        min_dist = min(min_dist, dist)
+        prev[word] = i
+    if min_dist == inf:
+        return -1
+    return min_dist
 
 
 if __name__ == '__main__':
