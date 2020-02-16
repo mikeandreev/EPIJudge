6b9db6eee7b5e1bea88d6df9cb69cf3fb7d302ac
Mihail Andreev <mikeandreev@gmail.com>
2020-02-16 23:21:03 +0300

    1 done

--- epi_judge_python/smallest_subarray_covering_set.py
+++ epi_judge_python/smallest_subarray_covering_set.py
@@ -16,7 +16,7 @@ def subarr_len(subarr):
     else:
         return subarr.end - subarr.start
 
-def find_smallest_subarray_covering_set(paragraph, keywords):
+def find_smallest_subarray_covering_set_v1(paragraph, keywords):
     subarr = Subarray(-1, -1)
     count = collections.Counter(keywords) # init value is 1
     empty_counter_val = 1
@@ -31,10 +31,9 @@ def find_smallest_subarray_covering_set(paragraph, keywords):
         count[word] += 1
 
         while i < j and remains_to_cover == 0:
-            if remains_to_cover == 0:
-                cur = Subarray(i, j-1)
-                if subarr_len(subarr) > subarr_len(cur):
-                    subarr = cur
+            cur = Subarray(i, j-1)
+            if subarr_len(subarr) > subarr_len(cur):
+                subarr = cur
 
             word, i = paragraph[i], i+1
             if word not in count:
@@ -43,7 +42,29 @@ def find_smallest_subarray_covering_set(paragraph, keywords):
             if count[word] == empty_counter_val:
                 remains_to_cover += 1
     return subarr
+    
+def find_smallest_subarray_covering_set_v2(paragraph, keywords):
+    from sortedcontainers import SortedSet
+    key_len, sorted_set = len(keywords), SortedSet()
+    res = Subarray(0, len(paragraph))
+    key_dict = {k: -1 for k in keywords }
+    for i, w in enumerate(paragraph):
+        if w not in key_dict: continue
+        
+        if key_dict[w] != -1: sorted_set.remove(key_dict[w])
+        
+        key_dict[w] = i
+        sorted_set.add(i)
+        if len(sorted_set) == key_len:
+            min_indx, max_indx = sorted_set[0], i
+            if res.end - res.start > max_indx - min_indx:
+                res = Subarray(min_indx, max_indx)
+                
+    if len(sorted_set) < key_len: return Subarray(-1, -1)
+    return res
 
+def find_smallest_subarray_covering_set(paragraph, keywords):
+    return find_smallest_subarray_covering_set_v1(paragraph, keywords)
 
 @enable_executor_hook
 def find_smallest_subarray_covering_set_wrapper(executor, paragraph, keywords):
